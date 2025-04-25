5f71075631db23c83126e722e13ddbab059137ca
Abhishek Anugu <158754433+Abhi-XD9@users.noreply.github.com>
2025-04-25 02:38:39 -0400

    Update App.jsx

--- xd/src/App.jsx
+++ xd/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Components/Home';
 import Image from './Components/Image';
@@ -8,12 +8,12 @@ import { ConversationProvider } from './Components/ConversationContext';
 function App() {
   return (
     <ConversationProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/image" element={<Image />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </ConversationProvider>
   );
 }
